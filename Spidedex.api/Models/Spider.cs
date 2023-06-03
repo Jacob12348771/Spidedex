2923db635d13624e76a4017a3269c0004f76bf2b
Jacob <jsmith@synergytaste.com>
2023-06-03 17:57:52 +0100

    Data access service complete

--- Spidedex.api/Models/Spider.cs
+++ Spidedex.api/Models/Spider.cs
@@ -6,11 +6,11 @@
     }
     public int Id { get; set; }
     public DateOnly DateObtained { get; set; }
-    public string Name { get; set; }
-    public string Species { get; set; }
-    public string Description { get; set; }
-    public string Size { get; set; }
-    public string Diet { get; set; }
-    public string UserInfo { get; set; }
+    public string ?Name { get; set; }
+    public string ?Species { get; set; }
+    public string ?Description { get; set; }
+    public string ?Size { get; set; }
+    public string ?Diet { get; set; }
+    public string ?UserInfo { get; set; }
     public Tempereament Temperament { get; set; }
 }
--- Spidedex/Services/DataAccessService.cs
+++ Spidedex/Services/DataAccessService.cs
@@ -1,32 +1,113 @@
-﻿using Spidedex.Model;
+﻿using Newtonsoft.Json;
+using Spidedex.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Spidedex.Services
 {
-    internal class DataAccessService : IDataAccessService
+    public class DataAccessService : IDataAccessService
     {
-        public Task<bool> AddUpdateSpiderAsync(Spider spider)
+        public readonly string _baseUri;
+        private readonly HttpClient _httpClient;
+        public DataAccessService()
         {
-            throw new NotImplementedException();
+            _baseUri = GetBaseAdress();
+            _httpClient = new() { BaseAddress = new Uri(_baseUri) };
         }
 
-        public Task<bool> DeleteSpiderAsync(int spiderId)
+        private string GetBaseAdress()
         {
-            throw new NotImplementedException();
+#if DEBUG
+            return DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5157" : "https://localhost:7160";
+            /*
+            #elif RELEASE
+                // published address here
+                return "https://BALH.azurewebsites.net";
+            */
+            #endif
+            
         }
 
-        public Task<Spider> GetSpiderAsync(int spiderId)
+        public async Task<bool> AddUpdateSpiderAsync(Spider spider)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (spider.Id == 0)
+                {
+                    var response = await _httpClient.PostAsync("/spiders", new StringContent(JsonConvert.SerializeObject(spider), Encoding.UTF8, "application/json"));
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await Task.FromResult(true);
+                    }
+                }
+                else
+                {
+                    var response = await _httpClient.PutAsync($"/spiders/{spider.Id}", new StringContent(JsonConvert.SerializeObject(spider), Encoding.UTF8, "application/json"));
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await Task.FromResult(true);
+                    }
+                }
+                return await Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(false);
+            }
         }
 
-        public Task<IEnumerable<Spider>> GetSpidersAsync(string userDetails)
+        public async Task<bool> DeleteSpiderAsync(int spiderId)
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.DeleteAsync($"/spiders/{spiderId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await Task.FromResult(true);
+            }
+            else
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<Spider> GetSpiderAsync(int spiderId)
+        {
+            var response = await _httpClient.GetAsync($"/spiders/{spiderId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return await Task.FromResult(JsonConvert.DeserializeObject<Spider>(content));
+            }
+            else
+            {
+                return await Task.FromResult<Spider>(null);
+            }
+        }
+
+        public async Task<IEnumerable<Spider>> GetSpidersAsync(string userDetails)
+        {
+            var spiderList = new List<Spider>();
+            if (userDetails != null)
+            {
+                var response = await _httpClient.GetAsync($"/spiders/foruser/{userDetails}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    spiderList = await response.Content.ReadFromJsonAsync<List<Spider>>();
+                }
+                return await Task.FromResult(spiderList);
+            }
+            else
+            {
+                return await Task.FromResult(spiderList);
+            }
         }
     }
 }
