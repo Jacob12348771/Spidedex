ea4a7d3e6004290f6da72913270da05d8edf0e62
Jacob <jsmith@synergytaste.com>
2023-06-27 20:14:24 +0100

    Various font changes.

--- Spidedex/MauiProgram.cs
+++ Spidedex/MauiProgram.cs
@@ -16,8 +16,8 @@ public static class MauiProgram
             .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Caveat-Regular.ttf", "Caveat-Regular");
+				fonts.AddFont("Caveat-SemiBold.ttf", "CaveatSemiBold");
 			});
 
 #if DEBUG
