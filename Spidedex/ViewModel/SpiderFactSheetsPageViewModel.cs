df4653238e168ded9dd4bac45b6bd25c18ef0e10
Jacob <jsmith@synergytaste.com>
2023-06-29 19:18:16 +0100

    Fact sheet cards showing.

--- Spidedex/ViewModel/SpiderFactSheetsPageViewModel.cs
+++ Spidedex/ViewModel/SpiderFactSheetsPageViewModel.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Networking;
+using Spidedex.Model;
+using Spidedex.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +15,44 @@ namespace Spidedex.ViewModel
 {
     public partial class SpiderFactSheetsPageViewModel : BaseViewModel
     {
+        IDataAccessService _dataAccessService;
+        public ObservableCollection<SpiderFactSheet> SpiderFactSheets { get; } = new();
+        public SpiderFactSheetsPageViewModel(IDataAccessService dataAccessService)
+        {
+            _dataAccessService = dataAccessService;
+        }
 
+        [ObservableProperty]
+        bool isRefreshing;
+
+        [RelayCommand]
+        async Task GetSpiderFactSheets()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+            try
+            {
+                IsBusy = true;
+
+                var factSheets = await _dataAccessService.GetSpiderFactSheetsAsync();
+
+                foreach (var factSheet in factSheets)
+                {
+                    SpiderFactSheets.Add(factSheet);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await AppShell.Current.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+                IsRefreshing = false;
+            }
+        }
     }
 }
